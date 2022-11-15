de30aa897f93d9296ef68fbc03b857bd2721b3e4
Fabyks <fabyks2015@gmail.com>
2022-11-15 10:18:58 +0530

    y first program

---
+++ imgsrc.c
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>ICET</title>
+</head>
+<body>
+<img src="/home/mca/img" ALT="luv image" WIDTH="40" HEIGHT="60" BORDER="4" HSPACE="30" VSPACE="10" ALIGN="LEFT">
+</body>
+</html>
